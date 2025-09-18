178d554ac47a43e3ccb5193f82bfbf8af1570812
Igor Souza <igornevesdesouza@gmail.com>
2025-09-17 23:25:12 -0300

    Minhas primeiras views, rotas e models

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    context = {
+        'curso': 'Programação Web com Django Framework',
+        'outro': 'Django é massa',
+       
+    }
+    return render(request,'index.html',context)
 
 def contato(request):
     return render(request,'contato.html')
